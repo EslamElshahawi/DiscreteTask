b970f501d44f4f182bc6e65a22348ba08a8ffe6b
EzlamElshahawi <79419183+EzlamElshahawi@users.noreply.github.com>
2022-12-21 04:53:08 +0200

    Add files via upload

---
+++ Project1/Program.cs
@@ -0,0 +1,42 @@
+﻿// This is a prime number checking app
+using System;
+namespace PrimeNumbers
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter starting number: ");
+            int startNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ending number: ");
+            int endNum = int.Parse(Console.ReadLine());
+            // Making number exception as prime numbers have to be positive
+            if (startNum <= 0 || endNum <= 0)
+            {
+                Console.WriteLine("Invalid Input!");
+                Console.ReadKey();
+            }
+            Console.WriteLine("These are the numbers between {0} and {1}", startNum, endNum);
+
+            for(int i = startNum; i <= endNum; i++)
+            {
+                int counter = 0;
+                for(int j = 2; j <= i/2; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        counter++;
+                        break;
+                    }
+                }
+
+                if(counter == 0 && i != 1)
+                {
+                    Console.WriteLine("{0} ", i);
+                }
+            }
+        }
+    }
+}
+ 
+
---
+++ Project2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace perfectNumbers
+{
+    public class program
+    {
+        public static void Main()
+        {
+            int sum;
+            int min, max;
+
+            Console.Write("Enter first number : ");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number : ");
+            max = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The Perfect numbers within the given range is : \n");
+            for (int i = min; i <= max; i++)
+            {
+                sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        sum += j;
+                }
+                if (sum == i)
+                    Console.WriteLine("{0} ", i);
+            }
+        }
+    }
+
+}
